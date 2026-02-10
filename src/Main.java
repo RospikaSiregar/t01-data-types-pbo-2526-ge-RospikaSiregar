2a92a5bb6672e3f56caa795f2146fe9e69124eca
RospikaSiregar <rospikasarahsiregar@gmail.com>
2026-02-10 09:59:11 +0700

    selesai

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // TODO: implement solution
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    try {
+        int hasil = Math.addExact(a, b);
+        System.out.println(hasil);
+    } catch (ArithmeticException e) {
+        System.out.println("OVERFLOW");
+    }
+
+    float a = sc.nextFloat ();
+    float b = sc.nextFloat ();
+    
     }
 }
